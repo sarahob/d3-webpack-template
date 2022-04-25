70daf161595d849c44b2426afccef93470b976b9
Sarah <sarahgobrien@gmail.com>
2022-04-25 11:59:13 +0200

    Initial commit of template.

---
+++ src/chart.js
@@ -0,0 +1,3 @@
+export default function drawChart() {
+  console.log("draw chart");
+}
---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    chart: "./src/chart.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "d3 webpack starter",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // regex to determine which files should run with this loader, all files ending with .css will be served to the style-loader and the css-loader
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
